f8d85b6c2f1e9579da1b3dd438930b0f08b8a903
lucien <lucien.edel@geops.de>
2020-02-27 12:12:15 +0100

    Updates

---
+++ precache-manifest.eb21db8a9e523dd6b87bd211292a6546.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baad6ccce89fbe2af9a59b3a01696fcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59a723de08ecdb76dbbf",
+    "url": "/static/css/2.90d703ac.chunk.css"
+  },
+  {
+    "revision": "b2f2b26b3bd14b27b80a",
+    "url": "/static/css/main.c110d7b7.chunk.css"
+  },
+  {
+    "revision": "59a723de08ecdb76dbbf",
+    "url": "/static/js/2.388ce3d4.chunk.js"
+  },
+  {
+    "revision": "346a39872e7c6831666322d6f45ae8b7",
+    "url": "/static/js/2.388ce3d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f2b26b3bd14b27b80a",
+    "url": "/static/js/main.5dffc9df.chunk.js"
+  },
+  {
+    "revision": "dbb8eba054a890ad8da5",
+    "url": "/static/js/runtime-main.27ad3dfc.js"
+  }
+]);
