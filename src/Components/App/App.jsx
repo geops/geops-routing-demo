b49e8db1fab62f255eafb0da29009ae008629dbf
lucien <lucien.edel@geops.de>
2020-02-27 10:05:09 +0100

    Remove relics folders/files

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -36,6 +36,7 @@ function App(props) {
         setApiKey(data.key);
       })
       .catch(() => {
+        // eslint-disable-next-line no-console
         console.error('Request to get the apiKey failed');
       });
     // eslint-disable-next-line react-hooks/exhaustive-deps
