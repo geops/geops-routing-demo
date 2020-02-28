ab832565d5b2d4c947a1516e23a1b0ee2b12a599
lucien <lucien.edel@geops.de>
2020-02-28 15:56:41 +0100

    Updates

--- precache-manifest.f2111706642944e8ac2a266ea4c29bf8.js
+++ precache-manifest.f2111706642944e8ac2a266ea4c29bf8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "47ecabe754ccc420f75e441adaf09fa7",
+    "revision": "9de51f3f2827673070649755879fe46d",
     "url": "/geops-routing-demo/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/geops-routing-demo/static/css/2.90d703ac.chunk.css"
   },
   {
-    "revision": "05c7c4aaf7788bb5a7e8",
+    "revision": "8edf75a1c7a7321107de",
     "url": "/geops-routing-demo/static/css/main.c93e70d7.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/geops-routing-demo/static/js/2.6f69ae4c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "05c7c4aaf7788bb5a7e8",
-    "url": "/geops-routing-demo/static/js/main.8469eae1.chunk.js"
+    "revision": "8edf75a1c7a7321107de",
+    "url": "/geops-routing-demo/static/js/main.6e1b5d70.chunk.js"
   },
   {
     "revision": "916339b31f6f5978a2b4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/geops-routing-demo/precache-manifest.819c1d5a2e57386830472779c4fe9fb9.js"
+  "/geops-routing-demo/precache-manifest.f2111706642944e8ac2a266ea4c29bf8.js"
 );
 
 self.addEventListener('message', (event) => {
