dc14c1c817f62fd932ed95b0c5ae00938af75e27
edellucien <edellucien@gmail.com>
2020-07-15 08:37:54 +0200

    Use stops v1 url

--- src/Components/Permalink/Permalink.jsx
+++ src/Components/Permalink/Permalink.jsx
@@ -65,11 +65,7 @@ const getGeoJson = (viaString, APIKey, stationSearchUrl) => {
 
   /* When the via is a UID */
   if (/^![a-zA-Z0-9]{16}$/.test(viaString)) {
-    // temporary use this url for lookup
-    reqUrl = `https://api.geops.io/stops/v1/lookup/${viaString.replace(
-      '!',
-      '',
-    )}/?key=${APIKey}`;
+    reqUrl = `${stationSearchUrl}lookup/${viaString.replace('!', '')}/?key=${APIKey}`;
   } else {
     /* search for the station. Remove ! in case it's an IBNR */
     reqUrl = `${stationSearchUrl}?q=${viaString.replace(
